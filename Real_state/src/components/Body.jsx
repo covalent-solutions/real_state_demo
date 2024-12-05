927f7869a9b49aac4fc38912987c96fbbd9149ab
yugalnyoupane <neunyouyugal12@gmail.com>
2024-12-05 17:42:47 +0545

    update

--- Real_state/src/components/Body.jsx
+++ Real_state/src/components/Body.jsx
@@ -1,16 +1,24 @@
 function Body() {
     return (
       <>
-        <div className="flex flex-col flex justify-center align-center  text-white text-lg" >
-            <div>
+        <div className="flex flex-col justify-center items-center text-white text-[25px] h-screen " >
+            <div className="p-8">
                 USA’s TOP REAL-ESTATE COMPANY FOR MORE THAN 30 YEARS
             </div>
             
             <div >
-                <button className="border border-white p-4">
+                <button className="border-2 border-white p-4">
                     FIND YOUR DREAM HOME
                 </button>
             </div>
+        </div>
+        <div className="flex flex-col justify-center items-center text-[30px]">
+          <div className="pb-4">
+            USA's TOP REAL-ESTATE COMPANY
+          </div>
+          <div>
+            FOR MORE THAN 30 YEARS
+          </div>
         </div>
       </>
     );
