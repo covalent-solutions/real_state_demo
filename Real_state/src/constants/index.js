457c3f8b51d8623eb160ce34126e8f2efda8a1ef
Roshan-Gupta-kly <lucysigma72@gmail.com>
2024-12-16 17:50:27 +0545

    made changes in tags, css properties

---
+++ Real_state/src/constants/index.js
@@ -0,0 +1,6 @@
+export const navLinks = [
+    { href: "#about", label: "About us" },
+    { href: "#properties", label: "Properties" },
+    { href: "#services", label: "Services" },
+    { href: "#contact", label: "Contact us" },
+  ];
